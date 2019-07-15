b0899e5bfe94780c15af25c737c6f19bbeb0bb2b
Junaid Ahmed <junaidkbr@gmail.com>
2019-07-16 03:50:40 +0500

    Rename data/ to dist/

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,4 @@
 (function () {
-    // const tasbih = require('../../data/tasbih.json');
     const main = document.querySelector('.tasbih');
 
     // load random tasbih
@@ -19,7 +18,7 @@
 function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', './data/tasbih.json', true); // Replace 'my_data' with the path to your file
+    xobj.open('GET', './dist/tasbih.json', true); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode

609b1baca1da8b763160b96501c39ec1c2c702c7
Junaid Ahmed <junaidkbr@gmail.com>
2019-07-16 03:43:51 +0500

    Add tasbih items in JSON

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,34 @@
-const main = document.querySelector('.zikr-container');
+(function () {
+    // const tasbih = require('../../data/tasbih.json');
+    const main = document.querySelector('.tasbih');
+
+    // load random tasbih
+    loadJSON(function (response) {
+        // Parse JSON string into object
+        const tasbih = JSON.parse(response);
+        const rand = Math.floor(Math.random() * tasbih.length);
+
+        console.log(tasbih);
+
+        console.log(rand);
+    });
+
+    // make text fit size of the container
+    // fitty('.tasbih', {
+    //     minSize: 100,
+    //     maxSize: 150
+    // });
+})();
+
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', './data/tasbih.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}

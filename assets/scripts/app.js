0b337cc90bdec5d020cedaa1ddf5bc70505b726d
Junaid Ahmed <junaidkbr@gmail.com>
2019-10-18 15:33:09 +0500

    Add getTasbihByID

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,19 +9,63 @@
             return response.json()
         })
         .then(function (tasbih) {
-            main.innerText = getRandomTasbih(tasbih);
+            window.tasbih = tasbih;
+            let id = getURLParam('id');
+
+            if (id) {
+                getTasbihByID(id);
+            } else {
+                main.innerHTML = getRandomTasbih();
+            }
         })
         .catch(function (error) {
             console.error(error);
         });
-
-    // make text fit size of the container
-    // fitty('.tasbih', {
-    //     minSize: 100,
-    //     maxSize: 250
-    // });
 })();
 
-function getRandomTasbih(tasbih) {
+/**
+ * Get random Tasbih
+ */
+function getRandomTasbih() {
+    if (typeof tasbih === 'undefined') {
+        return '';
+    }
+
     return tasbih[Math.floor(Math.random() * tasbih.length)].content
+}
+
+/**
+ * Get Tasbih by ID
+ *
+ * @param {String} id Tasbih ID
+ */
+function getTasbihByID(id) {
+    if (typeof tasbih === 'undefined' || typeof id === 'undefined' ) {
+        return '';
+    }
+
+    let t = null;
+
+    tasbih.forEach(function(temp) {
+        if (temp.id == id) {
+            t = temp;
+            return false;
+        }
+    });
+
+    return t;
+}
+
+function getURLParam(param) {
+    var query = window.location.search.substring(1);
+    var params = query.split("&");
+    for (var i = 0; i < params.length; i++) {
+        var pair = params[i].split("=");
+
+        if (pair[0] == param) {
+            return pair[1];
+        }
+    }
+
+    return (false);
 }

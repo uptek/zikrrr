74d523e417075e6efd15f225ba9ad57066947108
Shakil2022219 <shakilgi2222thub@gmail.com>
2020-06-02 02:09:09 +0500

    add 20 names of allah

--- src/js/main/data.js
+++ src/js/main/data.js
@@ -50,5 +50,110 @@ window.app['items'] = [
         "id": 10010,
         "content": "لا حَوْلَ وَلا قُوَّةَ إِلا بِالله",
         "category": []
+    },
+    {
+        "id": 10101,
+        "content": " الله",
+        "category": []
+    },
+    {
+        "id": 10102,
+        "content": "ٱلْرَّحْمَـانُ",
+        "category": []
+    },
+    {
+        "id": 10103,
+        "content": "ٱلْرَّحِيْمُ",
+        "category": []
+    },
+    {
+        "id": 10104,
+        "content": "ٱلْمَلِكُ",
+        "category": []
+    },
+    {
+        "id": 10105,
+        "content": "ٱلْقُدُّوسُ",
+        "category": []
+    },
+    {
+        "id": 10106,
+        "content": "ٱلْسَّلَامُ",
+        "category": []
+    },
+    {
+        "id": 10107,
+        "content": "ٱلْمُؤْمِنُ",
+        "category": []
+    },
+    {
+        "id": 10108,
+        "content": "ٱلْمُهَيْمِنُ",
+        "category": []
+    },
+    {
+        "id": 10109,
+        "content": "ٱلْعَزِيزُ",
+        "category": []
+    },
+    {
+        "id": 10110,
+        "content": "ٱلْجَبَّارُ",
+        "category": []
+    },
+    {
+        "id": 10110,
+        "content": "ٱلْمُتَكَبِّرُ",
+        "category": []
+    },
+    {
+        "id": 10111,
+        "content": "ٱلْخَالِقُ",
+        "category": []
+    },
+    {
+        "id": 10112,
+        "content": "ٱلْبَارِئُ",
+        "category": []
+    },
+    {
+        "id": 10113,
+        "content": "ٱلْمُصَوِّرُ",
+        "category": []
+    },
+    {
+        "id": 10114,
+        "content": "ٱلْغَفَّارُ",
+        "category": []
+    },
+    {
+        "id": 10115,
+        "content": "ٱلْقَهَّارُ",
+        "category": []
+    },
+    {
+        "id": 10116,
+        "content": "ٱلْوَهَّابُ",
+        "category": []
+    },
+    {
+        "id": 10117,
+        "content": "ٱلْرَّزَّاقُ",
+        "category": []
+    },
+    {
+        "id": 10118,
+        "content": "ٱلْفَتَّاحُ",
+        "category": []
+    },
+    {
+        "id": 10119,
+        "content": "ٱلْعَلِيمُ",
+        "category": []
+    },
+    {
+        "id": 10120,
+        "content": "ٱلْقَابِضُ",
+        "category": []
     }
 ];

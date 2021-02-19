4998e100dcb274509d08071b6a4ba8f9d86ff2a0
Muhammad Hasnain <mhasnaindev@gmail.com>
2021-02-20 01:06:28 +0500

    remove AJV and schema based testing

--- test/data.test.js
+++ test/data.test.js
@@ -1,50 +1,18 @@
 /* eslint-disable */
-const AJV = require('ajv').default;
-
 const data = require('../src/scripts/data/data.json');
 
 test('verifying tasbihaat data to be an array', () => {
   expect(Array.isArray(data)).toBe(true);
 });
 
-test('verifying tabihaat data to be an array of valid objects', () => {
-  const ajv = new AJV();
-
-  const validate = ajv.compile({
-    type: 'array',
-    items: {
-      type: 'object',
-      properties: {
-        id: {
-          type: 'integer',
-        },
-        content: {
-          type: 'string',
-        },
-        category: {
-          type: 'array',
-        },
-        transliteration: {
-          type: 'string',
-        },
-        translations: {
-          type: 'object',
-          properties: {
-            en: {
-              type: 'string',
-            },
-            ur: {
-              type: 'string',
-            },
-          },
-          required: ['en', 'ur'],
-          additionalProperties: false,
-        },
-      },
-      required: ['id', 'content', 'category', 'transliteration', 'translations'],
-      additionalProperties: false,
-    }
+data.forEach((tasbih) => {
+  test(`verifying tabih with id ${tasbih.id} to be a valid object`, () => {
+    expect(typeof tasbih.id).toBe('number');
+    expect(typeof tasbih.content).toBe('string');
+    expect(Array.isArray(tasbih.category)).toBe(true);
+    expect(typeof tasbih.transliteration).toBe('string');
+    expect(typeof tasbih.translations).toBe('object');
+    expect(typeof tasbih.translations.en).toBe('string');
+    expect(typeof tasbih.translations.ur).toBe('string');
   });
-
-  expect(validate(data)).toBe(true);
 });

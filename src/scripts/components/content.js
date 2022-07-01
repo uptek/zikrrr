986ceb8fe253479b0082c2073d286e7197675c2d
Junaid Ahmed <junaidkbr@gmail.com>
2022-07-01 05:53:16 +0500

    Improves code flexibility

--- src/scripts/components/content.js
+++ src/scripts/components/content.js
@@ -12,38 +12,32 @@ const DOMElements = {
   translationEN: document.querySelector(DOMStrings.translationEN),
 };
 
+const renderElement = (element, value) => {
+  if (!element) {
+    return;
+  }
+
+  if (!value.length) {
+    element.remove();
+    return;
+  }
+
+  /* eslint-disable no-param-reassign */
+  element.innerHTML = value;
+  element.title = value;
+  element.ariaLabel = value;
+  /* eslint-disable no-param-reassign */
+};
+
 export const updateContent = (tasbih) => {
-  if (typeof tasbih !== 'object' || !DOMElements.content) {
+  if (typeof tasbih !== 'object') {
     return;
   }
 
   DOMElements.container.dataset.id = tasbih.id;
-
-  if (tasbih.content.length) {
-    DOMElements.content.innerHTML = tasbih.content;
-    DOMElements.content.title = tasbih.content;
-    DOMElements.content.ariaLabel = tasbih.content;
-  } else {
-    DOMElements.content.remove();
-  }
-
-  if (tasbih.translations.ur.length) {
-    DOMElements.translationUR.innerHTML = tasbih.translations.ur;
-    DOMElements.translationUR.title = tasbih.translations.ur;
-    DOMElements.translationUR.ariaLabel = tasbih.translations.ur;
-  } else {
-    DOMElements.translationUR.remove();
-  }
-
-  if (tasbih.translations.en.length) {
-    DOMElements.translationEN.innerHTML = tasbih.translations.en;
-    DOMElements.translationEN.title = tasbih.translations.en;
-    DOMElements.translationEN.ariaLabel = tasbih.translations.en;
-  } else {
-    DOMElements.translationEN.remove();
-  }
-
-  console.log(tasbih);
+  renderElement(DOMElements.content, tasbih.content);
+  renderElement(DOMElements.translationUR, tasbih.translations.ur);
+  renderElement(DOMElements.translationEN, tasbih.translations.en);
 };
 
 export default updateContent;
